d3972a6a6342ff374c660aa0c3dbbca2d0822cf2
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2022-12-13 20:55:11 +0530

    Update Pass2.c

--- Pass2/Pass2.c
+++ Pass2/Pass2.c
@@ -6,14 +6,14 @@ int main()
 {
 	char a[10],ad[10],label[10],opcode[10],operand[10],symbol[10];
 	int start,diff,i,address,add,len,actual_len,finaddr,prevaddr,j=0;
-	char mnemonics[15][15]={"LDA","STA","LDCH","STCH"};
+	char mnemonic [15][15]={"LDA","STA","LDCH","STCH"};
 	char code[15][15]={"33","44","53","57"};
 	FILE *fp1,*fp2,*fp3,*fp4;
 	fp1=fopen("output.txt","w");
 	fp2=fopen("symtab.txt","r");
 	fp3=fopen("intermediate.txt","r");
 	fp4=fopen("objcode.txt","w");
-	fscanf(fp3,"\%s\t%s\t%s",label,opcode,operand);
+	fscanf(fp3,"%s\t%s\t%s",label,opcode,operand);
 	while(strcmp(opcode,"END")!=0)
 	{
 		prevaddr=address;
@@ -22,11 +22,11 @@ int main()
 	finaddr=address;
 	fclose(fp3);
 	fp3=fopen("intermediate.txt","r");
-	fscanf(fp3,"\t%s\t%s\t%s",label,opcode,operand);
+	fscanf(fp3,"%s\t%s\t%s",label,opcode,operand);
 	if(strcmp(opcode,"START")==0)
 	{
 		fprintf(fp1,"\t%s\t%s\t%s\n",label,opcode,operand);
-		fprintf(fp4,"H^%s^00%s^00%d\n",label,operand,finaddr);
+		fprintf(fp4,"H^%s00%s^00%d\n",label,operand,finaddr);
 		fscanf(fp3,"%d%s%s%s",&address,label,opcode,operand);
 		start=address;
 		diff=prevaddr-start;
@@ -42,7 +42,7 @@ int main()
 			fprintf(fp4,"^");
 			for(i=2;i<(actual_len+2);i++)
 			{
-				itoa(operand[i],ad,16);
+				sprintf(ad,"%x",operand[i]);
 				fprintf(fp1,"%s",ad);
 				fprintf(fp4,"%s",ad);
 			}
@@ -51,17 +51,17 @@ int main()
 		else if(strcmp(opcode,"WORD")==0)
 		{
 			len=strlen(operand);
-			itoa(atoi(operand),a,10);
+			sprintf(a,"%d",atoi(operand));
 			fprintf(fp1,"%d\t%s\t%s\t%s\t00000%s\n",address,label,opcode,operand,a);
 			fprintf(fp4,"^00000%s",a);
-		}	
+		}
 		else if((strcmp(opcode,"RESB")==0)||(strcmp(opcode,"RESW")==0))
 		{
 			fprintf(fp1,"%d\t%s\t%s\t%s\n",address,label,opcode,operand);
-		}
+		} 
 		else
 		{
-			while(strcmp(opcode,mnemonics[j])!=0)
+			while(strcmp(opcode,mnemonic[j])!=0)
 				j++;
 			if(strcmp(operand,"COPY")==0)
 				fprintf(fp1,"%d\t%s\t%s\t%s\t%s0000\n",address,label,opcode,operand,code[j]);
@@ -86,13 +86,12 @@ int main()
 	display();
 	return 0;
 }
-
 void display()
 {
 	char ch;
-	FILE *fp1,*fp2,*fp3,*fp4;
+	FILE *fp1, *fp2, *fp3,*fp4;
 	printf("\nIntermediate file is converted into object code");
-	printf("\n\nThe content of intermediate file:\n\n");
+	printf("\nThe contents of Intermediate file:\n\n");
 	fp3=fopen("intermediate.txt","r");
 	ch=fgetc(fp3);
 	while(ch!=EOF)
@@ -101,7 +100,7 @@ void display()
 		ch=fgetc(fp3);
 	}
 	fclose(fp3);
-	printf("\n\nThe content of Symbol Table: \n\n");
+	printf("\n\nThe contents of Symbol Table:\n\n");
 	fp2=fopen("symtab.txt","r");
 	ch=fgetc(fp2);
 	while(ch!=EOF)
@@ -110,7 +109,7 @@ void display()
 		ch=fgetc(fp2);
 	}
 	fclose(fp2);
-	printf("\n\nThe content of Output file:\n\n");
+	printf("\nThe contents of Output File:\n\n");
 	fp1=fopen("output.txt","r");
 	ch=fgetc(fp1);
 	while(ch!=EOF)
@@ -119,7 +118,7 @@ void display()
 		ch=fgetc(fp1);
 	}
 	fclose(fp1);
-	printf("\n\nThe content of Object code file:\n\n");
+	printf("\n\nThe contents of Object code File:\n\n");
 	fp4=fopen("objcode.txt","r");
 	ch=fgetc(fp4);
 	while(ch!=EOF)
